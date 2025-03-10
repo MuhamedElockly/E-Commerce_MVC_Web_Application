1209398d7d17210dda23345fe0237725c2a11113
dell <elocklymuhamed@gmail.com>
2025-03-10 22:41:14 +0200

    Add Product Repository

---
+++ Bulky.DataAccess/Repository/ProductRepsitory.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+	public class ProductRepsitory : Repository<Product>, IProductRepository
+	{
+		private readonly ApplicationDbContext _dbcontext;
+		public ProductRepsitory(ApplicationDbContext dbContext) : base(dbContext)
+		{
+			_dbcontext = dbContext;
+		}
+
+		public void Update(Product product)
+		{
+			_dbcontext.Products.Update(product);
+		}
+	}
+}
