0eb3389b39d454214c5210b9b88fddc509d74ccd
dell <elocklymuhamed@gmail.com>
2025-03-17 02:27:45 +0200

    complete detail page

--- MvcApp/Areas/Customer/Controllers/HomeController.cs
+++ MvcApp/Areas/Customer/Controllers/HomeController.cs
@@ -22,8 +22,13 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
+		public IActionResult Details(int productId)
+		{
+			Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
+			return View(product);
+		}
 
-        public IActionResult Privacy()
+		public IActionResult Privacy()
         {
             return View();
         }
