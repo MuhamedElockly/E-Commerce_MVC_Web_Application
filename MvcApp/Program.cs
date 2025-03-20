f793050cccb4194bdd915318b04cfda469b41bd9
dell <elocklymuhamed@gmail.com>
2025-03-21 00:21:49 +0200

    add identity tables into database

--- MvcApp/Program.cs
+++ MvcApp/Program.cs
@@ -14,7 +14,7 @@ namespace BulkyBookWeb
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
-   var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+   var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
@@ -27,6 +27,7 @@ namespace BulkyBookWeb
 				);
 
    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+		builder.Services.AddRazorPages();
 			builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
 
 			var app = builder.Build();
@@ -43,9 +44,9 @@ namespace BulkyBookWeb
 			app.UseStaticFiles();
 
 			app.UseRouting();
-
+			app.UseAuthentication();
 			app.UseAuthorization();
-
+			app.MapRazorPages();
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
