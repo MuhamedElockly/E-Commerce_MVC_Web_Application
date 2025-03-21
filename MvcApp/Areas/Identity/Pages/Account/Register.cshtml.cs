4063617c8ce9d992dbf138bf3a68d2ca2629036d
dell <elocklymuhamed@gmail.com>
2025-03-22 00:32:18 +0200

    finish user identity

--- MvcApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MvcApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -105,6 +105,13 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
 			public string ConfirmPassword { get; set; }
 			public string? Role { get; set; }
 			public IEnumerable<SelectListItem> RolesList { get; set; }
+			[Required]
+			public string Name { get; set; }	
+			public string? StreetAddress { get; set; }
+			public string? State { get; set; }
+			public string? City { get; set; }
+			public string? PostalCode { get; set; }
+			public string? PhoneNumber { get; set; }
 		}
 
 
@@ -138,6 +145,13 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
 
 				await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
 				await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+				user.PhoneNumber = Input.PhoneNumber;
+				user.City = Input.City;
+				user.Name=Input.Name;
+				user.State = Input.State;
+				user.PostalCode = Input.PostalCode;
+				user.StreetAddress = Input.StreetAddress;
+
 				var result = await _userManager.CreateAsync(user, Input.Password);
 
 				if (result.Succeeded)
@@ -184,7 +198,7 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
 			return Page();
 		}
 
-		private IdentityUser CreateUser()
+		private ApplicationUser CreateUser()
 		{
 			try
 			{
